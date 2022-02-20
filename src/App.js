2f5e59e8e8b5404cebc213ed31bee70ee4c2ee4d
TOUMIOmar <omartoumi158@gmail.com>
2022-02-20 03:38:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import Navbar from "./component/navbar";
+import Slideshow from "./component/About";
+import Info from "./component/info";
+import Reservation from "./component/Reservation";
+import Gall from "./component/Reservation";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <>
+        {/* <Navbar /> */}
+        <Slideshow/>
+
+
+        
+      </>
+      <Info/>
+    
+     
+      
+    </div>
+  );
+}
+
+export default App;
